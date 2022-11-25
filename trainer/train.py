04363db487510321983fb1665980f5756f0544e4
ZenaPoudel <37132766+ZenaPoudel@users.noreply.github.com>
2022-11-25 12:44:51 +0545

    Update train.py

--- trainer/train.py
+++ trainer/train.py
@@ -140,21 +140,21 @@ def main():
     plt.figure('train', (12,6))
     plt.subplot(1,2,1)
     plt.title("Epoch Average Loss")
-    x = [i+1 for i in range(len(args.epoch_loss_values))]
-    y = args.epoch_loss_values
+    x = [i+1 for i in range(len(epoch_loss_values))]
+    y = epoch_loss_values
     plt.xlabel('epoch')
     plt.plot(x, y)
     plt.show()
     plt.title("Training and Validation: Accuracy_curve")
-    x1 = [i+1 for i in range(len(args.epoch_train_accuracy_values))]
-    y1 = args.epoch_train_accuracy_values
+    x1 = [i+1 for i in range(len(epoch_train_accuracy_values))]
+    y1 = epoch_train_accuracy_values
     plt.xlabel('epoch')
     plt.plot(x1, y1)
     plt.show()
     hold on
     plt.title("Validation: Accuracy_Curve")
-    x2 = [(i+1) for i in range(len(args.metric_values))]
-    y2 = args.metric_values
+    x2 = [(i+1) for i in range(len(metric_values))]
+    y2 = metric_values
     plt.xlabel('epoch')
     plt.plot(x2,y2)
     plt.show() 		
