54acdcb81aea8d98cdcc5cf60b5d333492ce9078
ZenaPoudel <37132766+ZenaPoudel@users.noreply.github.com>
2023-01-07 11:35:25 +0545

    Update train.py

--- trainer/train.py
+++ trainer/train.py
@@ -50,7 +50,7 @@ def main():
 
 	# loss_function = torch.nn.BCEWithLogitsLoss()
 	loss_function = torch.nn.BCELoss()
-	optimizer = torch.optim.Adam(model.parameters(), args.learning_rate)
+	optimizer = torch.optim.Adam(model.parameters(), 0.0001)
 	val_interval = 1
 	best_metric = -1
 	best_metric_epoch = -1
@@ -59,175 +59,167 @@ def main():
 	train_accuracy = -1
 	epoch_loss_values = []
 	epoch_train_accuracy_values = []
+	epoch_train_balace_accuracy_values=[]
 	epoch_train_precision_values = []
 	epoch_train_recall_values = []
+	epoch_train_specificity_values = []
 	epoch_train_F1_values = []
 	epoch_val_accuracy_values = []
+	epoch_val_balace_accuracy_values=[]
 	epoch_val_precision_values = []
 	epoch_val_recall_values = []
+	epoch_val_specificity_values = []
 	epoch_val_F1_values = []
 	# writer = SummaryWriter()
-	max_epochs = args.epoch
+	max_epochs = 2
+
 
 	for epoch in range(max_epochs):
-		print("-" * 10)
-		print(f"epoch {epoch + 1}/{max_epochs}")
-		model.train()
-		epoch_loss = 0
-		epoch_train_accuracy = 0
-		epoch_train_precision = 0
-		epoch_train_recall= 0
-		epoch_train_F1 = 0
-		epoch_val_accuracy = 0
-		epoch_val_precision = 0
-		epoch_val_recall= 0
-		epoch_val_F1 = 0
-		step = 0
+	  y_pred = []
+	  y_true = []
+	  val_y_pred = []
+	  val_y_true = []
+	  print("-" * 10)
+	  print(f"epoch {epoch + 1}/{max_epochs}")
+	  model.train()
+	  epoch_loss = 0
+	  epoch_val_accuracy = 0
+	  epoch_val_precision = 0
+	  epoch_val_recall= 0
+	  epoch_val_F1 = 0
+	  step = 0
+	  for batch_data in train_loader:
+	    step += 1
+	    inputs, labe = batch_data[0].to(device), batch_data[1].to(device)
+	    optimizer.zero_grad()
+	    outputs = model(inputs)
+	    labels = []
+	    for i in labe:
+	      if i == 0:
+		i = [1,0]
+		labels.append(i)
+	      elif i == 1:
+		i = [0,1]
+		labels.append(i)
+	    label = np.array(labels)
+	    labels = torch.from_numpy(label)
+	    labels = labels.float().to(device)
+
+	# 			print(labels, outputs)
+
+	    loss = loss_function(outputs, labels)
+	    loss.backward()
+	    optimizer.step()
+	    epoch_loss += loss.item()
+	    epoch_len = len(train_ds) // train_loader.batch_size
 
 
-		for batch_data in train_loader:
-			step += 1
-			inputs, labe = batch_data[0].to(device), batch_data[1].to(device)
-			optimizer.zero_grad()
-			outputs = model(inputs)
-			labels = []
-			for i in labe:
-				if i == 0:
-					i = [1,0]
-					labels.append(i)
-				elif i == 1:
-					i = [0,1]
-					labels.append(i)
-			label = np.array(labels)
-			labels = torch.from_numpy(label)
-			labels = labels.float().to(device)
-			
-# 			print(labels, outputs)
+	# 			print(f"train loss: {loss.item():.4f}")
 
-			loss = loss_function(outputs, labels)
-			loss.backward()
-			optimizer.step()
-			epoch_loss += loss.item()
-			epoch_len = len(train_ds) // train_loader.batch_size
-			
-# 			print(f"train loss: {loss.item():.4f}")
-
-			accuracy = Accuracy()
-			accuracy.reset()
-			accuracy.update((outputs.argmax(dim=1), labe))
-			batch_acc = accuracy.compute()
-			epoch_train_accuracy += batch_acc
-
-			precision = Precision()
-			precision.reset()
-			precision.update((outputs.argmax(dim=1), labe))
-			batch_precision = precision.compute()
-			epoch_train_precision += batch_precision
-
-			recall = Recall()
-			recall.reset()
-			recall.update((outputs.argmax(dim=1), labe))
-			batch_recall = recall.compute()
-			epoch_train_recall += batch_recall
-
-			batch_F1 = (batch_precision * batch_recall * 2 / (batch_precision + batch_recall))
-			if batch_F1!=batch_F1:
-				batch_F1 = 0
-			else: 
-				batch_F1 =batch_F1.item()
-# 			print(batch_F1)
-
-			epoch_train_F1 += batch_F1
-
-			print(f"{step}/{epoch_len}, train loss: {loss.item():.4f}, train_accuracy: {batch_acc:.4f}, train_precision: {batch_precision.item():.4f}, train_recall: {batch_recall.item():.4f}, train_F1: {batch_F1:.4f}")
-
-		epoch_loss /= step
-		epoch_loss_values.append(epoch_loss)
-		epoch_train_accuracy /= step
-		epoch_train_accuracy_values.append(epoch_train_accuracy)
-		epoch_train_precision /= step
-		epoch_train_precision_values.append(epoch_train_precision)
-		epoch_train_recall /= step
-		epoch_train_recall_values.append(epoch_train_recall)
-		epoch_train_F1 /= step
-		epoch_train_F1_values.append(epoch_train_F1)
-		print(f"epoch {epoch + 1} average loss: {epoch_loss:.4f}, average accuracy: {epoch_train_accuracy:.4f}, average precision: { epoch_train_precision.item():.4f}, average recall: {epoch_train_recall.item():.4f}, average F1: {epoch_train_F1:.4f}")
+	    output = (torch.max(torch.exp(outputs), 1)[1]).data.cpu().numpy()
+	    y_pred.extend(output) # Save Prediction
 
 
-		if epoch_train_F1 > train_best_metric:
-			train_best_metric = epoch_train_F1
-			train_best_metric_epoch = epoch + 1
-			# torch.save(model.state_dict(), "best_metric_model_classification3d_array.pth")
-			# print("saved new training best metric model")
-		print(f"Best train F1 score: {train_best_metric:.4f} at epoch {train_best_metric_epoch}")
+	    labels = labe.data.cpu().numpy()
+	    y_true.extend(labels) # Save Truth
 
-		if (epoch + 1) % val_interval == 0:
-			model.eval()
-			num_correct = 0.0
-			metric_count = 0
-			val_step = 0
-			for val_data in val_loader:
-				val_step +=1
-				val_images, val_labels = val_data[0].to(device), val_data[1].to(device)
-				with torch.no_grad():
-					val_outputs = model(val_images)
-					val_accuracy = Accuracy()
-					val_accuracy.reset()
-					val_accuracy.update((val_outputs, val_labels))
-					val_batch_acc = val_accuracy.compute()
-					epoch_val_accuracy += val_batch_acc
 
-					val_precision = Precision()
-					val_precision.reset()
-					val_precision.update((val_outputs.argmax(dim=1), val_labels))
-					val_batch_precision = val_precision.compute()
-					epoch_val_precision += val_batch_precision
+	  epoch_loss /= step
+	  epoch_loss_values.append(epoch_loss)
 
-					val_recall = Recall()
-					val_recall.reset()
-					val_recall.update((val_outputs.argmax(dim=1), val_labels))
-					val_batch_recall = val_recall.compute()
-					epoch_val_recall += val_batch_recall
+	  tn, fp, fn, tp = confusion_matrix(y_true, y_pred).ravel()
 
-					val_batch_F1 = (val_batch_precision * val_batch_recall * 2 / (val_batch_precision + val_batch_recall))
-					if val_batch_F1!=val_batch_F1:
-						val_batch_F1 = 0
-					else: 
-						val_batch_F1 =val_batch_F1.item()
+	  acc = accuracy_score(y_true, y_pred)
+	  epoch_train_accuracy_values.append(acc)
 
-					epoch_val_F1 += val_batch_F1
-					print(f"{val_step}, val_accuracy: {val_batch_acc:.4f}, val_precision: {val_batch_precision.item():.4f}, val_recall: {val_batch_recall.item():.4f}, val_F1: {val_batch_F1:.4f}")
 
-			epoch_val_accuracy /= val_step
-			epoch_val_accuracy_values.append(epoch_val_accuracy)
-			epoch_val_precision /= val_step
-			epoch_val_precision_values.append(epoch_val_precision)
-			epoch_val_recall /= val_step
-			epoch_val_recall_values.append(epoch_val_recall)
-			epoch_val_F1 /= val_step
-			epoch_val_F1_values.append(epoch_val_F1)
+	  bal_acc= balanced_accuracy_score(y_true, y_pred)
+	  epoch_train_balace_accuracy_values.append(bal_acc)
 
-			if epoch_val_F1 > best_metric:
-				best_metric = epoch_val_F1
-				best_metric_epoch = epoch + 1
-				torch.save(model.state_dict(), "best_metric_model_classification3d_array.pth")
-				print("saved new validation best metric model")
+	  precision = precision_score(y_true, y_pred)
+	  epoch_train_precision_values.append(precision)
 
-			print(f"val accuracy: {epoch_val_accuracy:.4f}, val precision: { epoch_val_precision.item():.4f}, val recall: {epoch_val_recall.item():.4f}, val F1: {epoch_val_F1:.4f}")
-			print(f"Best validation F1 score: {best_metric:.4f} at epoch {best_metric_epoch}")
-		# # writer.add_scalar("val_accuracy", metric, epoch + 1)
+	  recall= recall_score(y_true, y_pred)
+	  epoch_train_precision_values.append(recall)
 
-	print(f"Training completed, training best_metric: {train_best_metric:.4f} at epoch: {train_best_metric_epoch}, validation best_metric: {best_metric:.4f} at epoch: {best_metric_epoch}")
+	  specificity = tn / (tn+fp)
+	  epoch_train_specificity_values.append(specificity)
 
-	
-	
+	  f1_score = (precision * recall * 2 )/ (precision +recall)
+
+	  if f1_score!=f1_score:
+	    f1_score = 0
+	  else: 
+	    f1_score =f1_score
+
+	  epoch_train_F1_values.append(f1_score)
+
+	  print(f"epoch {epoch + 1} average epoch loss: {epoch_loss:.4f}, epoch train accuracy: {acc:.4f}, epoch train balanced_Acc:{bal_acc:.4f} epoch train precision: { precision:.4f}, epoch train recall: {recall:.4f}, epoch train F1: {f1_score:.4f}, epoch train specificity: {specificity:.4f}, epoch train auc score: {roc_auc_score(y_true, y_pred)}")
+
+	  if (epoch + 1) % val_interval == 0:
+	    model.eval()
+	    with torch.no_grad():
+	      num_correct = 0.0
+	      metric_count = 0
+	      val_step = 0
+	      for val_data in val_loader:
+		val_step +=1
+		val_images, val_labels = val_data[0].to(device), val_data[1].to(device)
+		val_outputs = model(val_images)
+
+		val_output = (torch.max(torch.exp(val_outputs), 1)[1]).data.cpu().numpy()
+		val_y_pred.extend(val_output) # Save Prediction
+
+
+		val_labels = val_labels.data.cpu().numpy()
+		val_y_true.extend(val_labels) # Save Truth
+
+	    vtn, vfp, vfn, vtp = confusion_matrix(val_y_true, val_y_pred).ravel()
+
+	    val_acc = accuracy_score(val_y_true, val_y_pred)
+	    epoch_val_accuracy_values.append(val_acc)
+
+
+	    val_bal_acc= balanced_accuracy_score(val_y_true, val_y_pred)
+	    epoch_val_balace_accuracy_values.append(val_bal_acc)
+
+	    val_precision = precision_score(val_y_true, val_y_pred)
+	    epoch_val_precision_values.append(val_precision)
+
+	    val_recall= recall_score(val_y_true, val_y_pred)
+	    epoch_val_precision_values.append(val_recall)
+
+	    val_specificity = vtn / (vtn+vfp)
+	    epoch_val_specificity_values.append(val_specificity)
+
+	    val_f1_score = (val_precision * val_recall * 2 )/ (val_precision + val_recall)
+
+	    if val_f1_score!=val_f1_score:
+	      val_f1_score = 0
+	    else: 
+	      val_f1_score =val_f1_score
+
+	    epoch_val_F1_values.append(val_f1_score)
+
+	    if val_f1_score > best_metric:
+	      best_metric = val_f1_score
+	      best_metric_epoch = epoch + 1
+	      torch.save(model.state_dict(), "best_metric_model_classification3d_array.pth")
+	      print("saved new validation best metric model")
+
+	    print(f"epoch {epoch + 1}, val confusion matrix:{(vtn, vfp, vfn, vtp)} epoch val accuracy: {val_acc:.4f}, epoch val balanced_Acc:{val_bal_acc:.4f} epoch precision: { val_precision:.4f}, epoch recall: {val_recall:.4f}, epoch F1: {val_f1_score:.4f}, epoch specificity: {val_specificity:.4f}, epoch val auc score: {roc_auc_score(val_y_true, val_y_pred)}")
+
+	print(f"Training completed, validation best_metric: {best_metric:.4f} at epoch: {best_metric_epoch}")
+
+
+
 
 
 	# customizing runtime configuration stored
 	# in matplotlib.rcParams
 	plt.rcParams["figure.figsize"] = [7.00, 3.50]
 	plt.rcParams["figure.autolayout"] = True
-	
+
 	train_loss_acc = plt.figure('loss_acc', (12,6))
 	plt.subplot(1,2,1)
 	plt.title("Epoch Average Loss and accuracy")
@@ -244,26 +236,26 @@ def main():
 
 	plt.legend(loc='upper left')
 	plt.savefig('TrainingandValidationAccurccy.png')
-	
+
 	F1_score = plt.figure('F1', (12,6))
 	plt.subplot(1,2,1)
-	plt.title("Epoch Average Loss")
-	
+
+	plt.title("Epoch Average f1")
+
 	x2 = [i+1 for i in range(len(epoch_train_F1_values))]
 	y2 = epoch_train_F1_values
-	
+
 	x4 = [i+1 for i in range(len(epoch_val_F1_values))]
 	y4 = epoch_val_F1_values
-	
-	
+
+
 	plt.plot(x2, y2, label='Training F1 score')
 	plt.plot(x4, y4, label='validation_F1_score')
-	
+
 	plt.title('Training and Validation F1 score')
 
 	plt.legend(loc='upper left')
 	plt.savefig('TrainingandValidationF1.png')
-	
 
 if __name__ == '__main__':
 	main()
