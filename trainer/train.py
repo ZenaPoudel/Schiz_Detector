2192132556bc140d40807fef52e836fe596264ec
ZenaPoudel <37132766+ZenaPoudel@users.noreply.github.com>
2023-01-07 12:40:05 +0545

    Update train.py

--- trainer/train.py
+++ trainer/train.py
@@ -95,11 +95,11 @@ def main():
 	    labels = []
 	    for i in labe:
 	      if i == 0:
-					i=[1,0]
-					labels.append(i)
+		i=[1,0]
+		labels.append(i)
 	      elif i == 1:
-					i = [0,1]
-					labels.append(i)
+		i = [0,1]
+		labels.append(i)
 	    label = np.array(labels)
 	    labels = torch.from_numpy(label)
 	    labels = labels.float().to(device)
