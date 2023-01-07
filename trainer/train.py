bf835004b7339639feb5924d6fa0fc61d429952a
ZenaPoudel <37132766+ZenaPoudel@users.noreply.github.com>
2023-01-07 13:20:23 +0545

    Update train.py

--- trainer/train.py
+++ trainer/train.py
@@ -22,8 +22,8 @@ np.random.seed(random_seed)
 device = torch.device("cuda" ) if torch.cuda.is_available() else "cpu"
 
 def main():
-	parser = argparse.ArgumentParser(description='Base_paper 1 3D convolution for action recognition')
-	parser.add_argument('--batch_size', type=int, default=8)
+  parser = argparse.ArgumentParser(description='Base_paper 1 3D convolution for action recognition')
+  parser.add_argument('--batch_size', type=int, default=8)
 	parser.add_argument('--epoch', type=int, default=50)
 	parser.add_argument('--learning_rate', type=float, default=0.0001)
 	parser.add_argument('--dropout', type=float, default=0.3)
@@ -34,11 +34,9 @@ def main():
 	parser.add_argument('--resize_spatial_size', type=str, default='99,99,99')
 	parser.add_argument('--test_split', type=float, default=0.3)
 	parser.add_argument('--ratio', type=str, default='NO')
-
-	args = parser.parse_args()
+  args = parser.parse_args()
 	resize_spatial_size = [int(item) for item in args.resize_spatial_size.split(',')]
-	train_ds, train_loader, val_loader = data_pull_and_load(
-	      data_source= args.data_source, 
+	train_ds, train_loader, val_loader = data_pull_and_load(	      data_source= args.data_source, 
 	      mri_type = args.mri_type, 
 	      pix_dimension=(args.pix_dimension[0], args.pix_dimension[1], args.pix_dimension[2]), 
 	      resize_spatial_size=(resize_spatial_size[0], resize_spatial_size[1], resize_spatial_size[2]),
@@ -46,11 +44,9 @@ def main():
 	      batch_size=args.batch_size,
 	      ratio=args.ratio
 	  )
-	model = model_3DCNN(dropout = args.dropout)
-
-	# loss_function = torch.nn.BCEWithLogitsLoss()
+  model = model_3DCNN(dropout = args.dropout)
 	loss_function = torch.nn.BCELoss()
-	optimizer = torch.optim.Adam(model.parameters(), 0.0001)
+	optimizer = torch.optim.Adam(model.parameters(), args.learning_rate)
 	val_interval = 1
 	best_metric = -1
 	best_metric_epoch = -1
@@ -71,60 +67,52 @@ def main():
 	epoch_val_specificity_values = []
 	epoch_val_F1_values = []
 	# writer = SummaryWriter()
-	max_epochs = 2
-
-
-	for epoch in range(max_epochs):
-	  y_pred = []
-	  y_true = []
-	  val_y_pred = []
-	  val_y_true = []
-	  print("-" * 10)
-	  print(f"epoch {epoch + 1}/{max_epochs}")
-	  model.train()
-	  epoch_loss = 0
-	  epoch_val_accuracy = 0
-	  epoch_val_precision = 0
-	  epoch_val_recall= 0
-	  epoch_val_F1 = 0
-	  step = 0
-	  for batch_data in train_loader:
-	    step += 1
-	    inputs, labe = batch_data[0].to(device), batch_data[1].to(device)
-	    optimizer.zero_grad()
-	    outputs = model(inputs)
-	    labels = []
-	    for i in labe:
-	      if i == 0:
-		i=[1,0]
-		labels.append(i)
-	      elif i == 1:
-		i=[0,1]
-		labels.append(i)
+	max_epochs = args.epoch
+  for epoch in range(max_epochs):
+    y_pred = []
+    y_true = []
+    val_y_pred = []
+    val_y_true = []
+    print("-" * 10)
+    print(f"epoch {epoch + 1}/{max_epochs}")
+    model.train()
+    epoch_loss = 0
+    epoch_val_accuracy = 0
+    epoch_val_precision = 0
+    epoch_val_recall= 0
+    epoch_val_F1 = 0
+    step = 0
+    for batch_data in train_loader:
+      step += 1
+      inputs, labe = batch_data[0].to(device), batch_data[1].to(device)
+      optimizer.zero_grad()
+      outputs = model(inputs)
+      labels = []
+      for i in labe:
+        if i == 0:
+          i=[1,0]
+          labels.append(i)
+        elif i == 1:
+          i=[0,1]
+          labels.append(i)
 	    label = np.array(labels)
 	    labels = torch.from_numpy(label)
 	    labels = labels.float().to(device)
-
-	# 			print(labels, outputs)
-
-	    loss = loss_function(outputs, labels)
+      
+      loss = loss_function(outputs, labels)
 	    loss.backward()
 	    optimizer.step()
 	    epoch_loss += loss.item()
 	    epoch_len = len(train_ds) // train_loader.batch_size
-
-
-	# 			print(f"train loss: {loss.item():.4f}")
-
-	    output = (torch.max(torch.exp(outputs), 1)[1]).data.cpu().numpy()
+      
+      output = (torch.max(torch.exp(outputs), 1)[1]).data.cpu().numpy()
 	    y_pred.extend(output) # Save Prediction
 
 
 	    labels = labe.data.cpu().numpy()
-	    y_true.extend(labels) # Save Truth
-
-
-	  epoch_loss /= step
+	    y_true.extend(labels)
+      
+    epoch_loss /= step
 	  epoch_loss_values.append(epoch_loss)
 
 	  tn, fp, fn, tp = confusion_matrix(y_true, y_pred).ravel()
@@ -154,27 +142,27 @@ def main():
 
 	  epoch_train_F1_values.append(f1_score)
 
-	  print(f"epoch {epoch + 1} average epoch loss: {epoch_loss:.4f}, epoch train accuracy: {acc:.4f}, epoch train balanced_Acc:{bal_acc:.4f} epoch train precision: { precision:.4f}, epoch train recall: {recall:.4f}, epoch train F1: {f1_score:.4f}, epoch train specificity: {specificity:.4f}, epoch train auc score: {roc_auc_score(y_true, y_pred)}")
+	  print(f"epoch {epoch + 1}, training, average loss: {epoch_loss:.4f}, accuracy: {acc:.4f}, balanced_Acc:{bal_acc:.4f}, precision: { precision:.4f}, recall: {recall:.4f}, F1: {f1_score:.4f}, specificity: {specificity:.4f}, auc score: {roc_auc_score(y_true, y_pred)}")
 
 	  if (epoch + 1) % val_interval == 0:
-	    model.eval()
+      model.eval()
 	    with torch.no_grad():
 	      num_correct = 0.0
 	      metric_count = 0
 	      val_step = 0
 	      for val_data in val_loader:
-			val_step +=1
-			val_images, val_labels = val_data[0].to(device), val_data[1].to(device)
-			val_outputs = model(val_images)
+          val_step +=1
+          val_images, val_labels = val_data[0].to(device), val_data[1].to(device)
+          val_outputs = model(val_images)
 
-			val_output = (torch.max(torch.exp(val_outputs), 1)[1]).data.cpu().numpy()
-			val_y_pred.extend(val_output) # Save Prediction
+          val_output = (torch.max(torch.exp(val_outputs), 1)[1]).data.cpu().numpy()
+          val_y_pred.extend(val_output) # Save Prediction
 
 
-			val_labels = val_labels.data.cpu().numpy()
-			val_y_true.extend(val_labels) # Save Truth
-
-	    vtn, vfp, vfn, vtp = confusion_matrix(val_y_true, val_y_pred).ravel()
+          val_labels = val_labels.data.cpu().numpy()
+          val_y_true.extend(val_labels) # Save Truth
+          
+      vtn, vfp, vfn, vtp = confusion_matrix(val_y_true, val_y_pred).ravel()
 
 	    val_acc = accuracy_score(val_y_true, val_y_pred)
 	    epoch_val_accuracy_values.append(val_acc)
@@ -195,7 +183,7 @@ def main():
 	    val_f1_score = (val_precision * val_recall * 2 )/ (val_precision + val_recall)
 
 	    if val_f1_score!=val_f1_score:
-	      val_f1_score = 0
+        val_f1_score = 0
 	    else: 
 	      val_f1_score =val_f1_score
 
@@ -207,15 +195,11 @@ def main():
 	      torch.save(model.state_dict(), "best_metric_model_classification3d_array.pth")
 	      print("saved new validation best metric model")
 
-	    print(f"epoch {epoch + 1}, val confusion matrix:{(vtn, vfp, vfn, vtp)} epoch val accuracy: {val_acc:.4f}, epoch val balanced_Acc:{val_bal_acc:.4f} epoch precision: { val_precision:.4f}, epoch recall: {val_recall:.4f}, epoch F1: {val_f1_score:.4f}, epoch specificity: {val_specificity:.4f}, epoch val auc score: {roc_auc_score(val_y_true, val_y_pred)}")
-
-	print(f"Training completed, validation best_metric: {best_metric:.4f} at epoch: {best_metric_epoch}")
-
-
-
-	# customizing runtime configuration stored
-	# in matplotlib.rcParams
-	plt.rcParams["figure.figsize"] = [7.00, 3.50]
+      print(f"epoch {epoch + 1}, validation, confusion matrix:{(vtn, vfp, vfn, vtp)} accuracy: {val_acc:.4f}, balanced accuracy:{val_bal_acc:.4f} precision: { val_precision:.4f}, recall: {val_recall:.4f}, F1: {val_f1_score:.4f}, specificity: {val_specificity:.4f}, auc score: {roc_auc_score(val_y_true, val_y_pred)}")
+    
+  print(f"Training completed, validation best_metric: {best_metric:.4f} at epoch: {best_metric_epoch}")
+    
+  plt.rcParams["figure.figsize"] = [7.00, 3.50]
 	plt.rcParams["figure.autolayout"] = True
 
 	train_loss_acc = plt.figure('loss_acc', (12,6))
@@ -257,3 +241,4 @@ def main():
 
 if __name__ == '__main__':
 	main()
+
